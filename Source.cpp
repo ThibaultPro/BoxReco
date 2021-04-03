f953ef881a633c6f7e29fb9940983235a2a27080
ThibaultPro <thibault.pierga@gmail.com>
2021-04-03 18:53:58 +0200

    adding homography calculation

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,8 @@ using namespace cv;
 
 
 Mat getColorRangeImage(Mat);
-
+vector<Point2f> findPointsWithTransfo(Mat source);
+vector<Point2f> selectGoodPointsCam1(vector<Point> cnts_poly);
 
 /**
 Mat getColorRangeImage(Mat color_img)
@@ -87,18 +88,18 @@ vector<Point> findPointsNoTransfo(string image)
 	return cnts_poly;
 }
 
-vector<Point> findPointsWithTransfo(string image)
+vector<Point2f> findPointsWithTransfo(Mat source)
 {
-	Mat source, hsv, proc_img, canny, dest1, dest2;
+	Mat hsv, proc_img, canny, dest1, dest2;
 	Mat struct_element0, struct_element1, struct_element2;
 	string directory = "goodPictures";
 
 	vector<vector<Point>> contours;
 	vector<Point> cnts_poly;
+	vector<Point2f> goodPoints;
 
 	if (!source.empty())
 	{
-		cerr << "couldn't open the image";
 
 		cvtColor(source, source, COLOR_BGR2YCrCb); //2Luv, 2XYZ, 2YUV 
 
@@ -143,18 +144,19 @@ vector<Point> findPointsWithTransfo(string image)
 
 		//this function approximate points of a polygon for the contour
 		approxPolyDP(Mat(contours[indexMax]), cnts_poly, 3, true);
+		goodPoints = selectGoodPointsCam1(cnts_poly);
 	}
-	return cnts_poly;
+	return goodPoints;
 }
 
-vector<Point> selectGoodPointsCam1(vector<Point> cnts_poly)
+vector<Point2f> selectGoodPointsCam1(vector<Point> cnts_poly)
 {
 	double MaxY = cnts_poly[0].y;
 	double MinY = cnts_poly[0].y;
 	double MinX = cnts_poly[0].x;
 	double MaxX = cnts_poly[0].x;
 
-	vector<Point> good_points;
+	vector<Point2f> good_points;
 
 	for (int i = 0; i < cnts_poly.size(); i++)
 	{
@@ -188,7 +190,6 @@ vector<Point> selectGoodPointsCam1(vector<Point> cnts_poly)
 
 	for (int i = 0; i < cnts_poly.size(); i++)
 	{
-		cout << MaxY << endl;
 		if (abs(cnts_poly[i].y - MaxY) < 10)
 		{
 			for (int j = 0; j < cnts_poly.size(); j++)
@@ -234,13 +235,13 @@ vector<Point> selectGoodPointsCam1(vector<Point> cnts_poly)
 		}
 		
 	}
-	good_points.push_back(cnts_poly[index_p31]);
-	good_points.push_back(cnts_poly[index_p32]);
+	good_points.push_back(Point2f(cnts_poly[index_p31].x, cnts_poly[index_p31].y));
+	good_points.push_back(Point2f(cnts_poly[index_p32].x, cnts_poly[index_p32].y));
 
-	good_points.push_back(cnts_poly[index_p21]);
-	good_points.push_back(cnts_poly[index_p22]);
-	good_points.push_back(cnts_poly[index_p11]);
-	good_points.push_back(cnts_poly[index_p12]);
+	good_points.push_back(Point2f(cnts_poly[index_p21].x, cnts_poly[index_p21].y));
+	good_points.push_back(Point2f(cnts_poly[index_p22].x, cnts_poly[index_p22].y));
+	good_points.push_back(Point2f(cnts_poly[index_p11].x, cnts_poly[index_p11].y));
+	good_points.push_back(Point2f(cnts_poly[index_p12].x, cnts_poly[index_p12].y));
 
 	return good_points;
 }
@@ -302,7 +303,6 @@ vector<Point> selectGoodPointsCam2(vector<Point> cnts_poly)
 			}
 		}
 
-
 		if ((abs(cnts_poly[i].x - cnts_poly[index_p11].x) < 20) && (abs(cnts_poly[i].y - cnts_poly[index_p11].y) > 50))
 		{
 			index_p21 = i;
@@ -328,11 +328,13 @@ void work_on_points()
 		if (i != 5)
 		{
 			string image1 = "carton_" + to_string(i + 1) + "_pos1.jpg";
-			vector<Point> foundPoints1, foundPoints2;
+			vector<Point> foundPoints1;
+			vector<Point> foundPoints2;
 			foundPoints1 = findPointsNoTransfo(image1);
 
-			string image2 = "carton_" + to_string(i + 1) + "_pos2.jpg";
-			foundPoints2 = findPointsWithTransfo(image2);
+			string image2_string = "carton_" + to_string(i + 1) + "_pos2.jpg";
+			Mat image2 = imread(image2_string);
+			//foundPoints2 = findPointsWithTransfo(image2);
 
 		}
 	}
@@ -341,7 +343,7 @@ void work_on_points()
 int treat_and_display_with_transfo()
 {
 
-	Mat source, hsv, proc_img, canny, dest1, dest2;
+	Mat source, hsv, canny, dest1, dest2;
 	Mat struct_element0, struct_element1, struct_element2;
 	string file = "goodPictures/carton_1_pos2.jpg";
 
@@ -378,12 +380,14 @@ int treat_and_display_with_transfo()
 	cvtColor(source, source, COLOR_BGR2YCrCb); //2Luv, 2XYZ, 2YUV
 
 
-	proc_img = getColorRangeImage(source);
+	Mat proc_img = Mat(source.size(), CV_8UC3);
+
+	inRange(source, Scalar(150, 130, 102), Scalar(215, 165, 120), proc_img);
 
 	struct_element0 = getStructuringElement(MORPH_RECT, Size(5, 5), Point(1, 1));
 	struct_element1 = getStructuringElement(MORPH_RECT, Size(15, 15), Point(3, 3));
 
-	struct_element2 = getStructuringElement(MORPH_RECT, Size(12, 12), Point(3, 3));
+	struct_element2 = getStructuringElement(MORPH_RECT, Size(11, 11), Point(3, 3));
 
 	//morphological operations to eliminate most noise on the box 
 	morphologyEx(proc_img, proc_img, MORPH_CLOSE, struct_element0);
@@ -426,15 +430,15 @@ int treat_and_display_with_transfo()
 
 	fillPoly(dest1, contours[indexMax], Scalar(255, 255, 255));
 
+
 	drawContours(dest2, cnts, 0, Scalar(0, 0, 255), 5);
 
 	//cornerHarris(dest2, corners, 2, 3, 0.04);
 	//goodFeaturesToTrack(dest2, corners, 1, 0.5, 50, noArray(), 3, true);
 
-
 	//display the points of the polygons. This is what we use to estimate the dimensions
 
-	vector<Point> gPoints;
+	vector<Point2f> gPoints;
 	gPoints = selectGoodPointsCam1(cnts_poly);
 	for (int j = 0; j < gPoints.size(); j++)
 	{
@@ -456,7 +460,7 @@ int treat_and_display_with_transfo()
 int treat_and_display_with_no_transfo()
 {
 	Mat source, hsv, proc_img, canny, dest1, dest2;
-	Mat struct_element0, struct_element1, struct_element2;
+	Mat struct_element0, struct_element1, struct_element2, struct_element3;
 	string directory = "goodPictures";
 	string file = "goodPictures/carton_1_pos1.jpg";
 
@@ -481,25 +485,28 @@ int treat_and_display_with_no_transfo()
 
 	source = imread(file);
 
+
 	if (!source.empty())
 	{
-
+		cvtColor(source, source, COLOR_BGR2YCrCb);
 		proc_img = Mat(source.size(), CV_8UC3);
 
-		inRange(source, Scalar(80, 130, 150), Scalar(210, 215, 230), proc_img);
+		inRange(source, Scalar(150, 110, 85), Scalar(240, 210, 130), proc_img);
 
-		struct_element0 = getStructuringElement(MORPH_RECT, Size(5, 5), Point(1, 1));
+		struct_element0 = getStructuringElement(MORPH_RECT, Size(5, 5), Point(3, 3));
 
-		struct_element1 = getStructuringElement(MORPH_RECT, Size(7, 7), Point(3, 3));
+		struct_element1 = getStructuringElement(MORPH_RECT, Size(9, 9), Point(3, 3));
 
 		struct_element2 = getStructuringElement(MORPH_RECT, Size(7, 7), Point(3, 3));
+		struct_element3 = getStructuringElement(MORPH_RECT, Size(7, 7), Point(3, 3));
 
 		//morphological operations to eliminate most noise on the box 
 		morphologyEx(proc_img, proc_img, MORPH_CLOSE, struct_element0);
 
-		erode(proc_img, proc_img, struct_element1);
 		dilate(proc_img, proc_img, struct_element2);
+		erode(proc_img, proc_img, struct_element1);
 
+		//dilate(proc_img, proc_img, struct_element3);
 		
 		Canny(proc_img, canny, 100, 200);
 
@@ -532,19 +539,18 @@ int treat_and_display_with_no_transfo()
 		cnts.push_back(cnts_poly);
 
 		fillPoly(dest1, contours[indexMax], Scalar(255, 255, 255));
+		imwrite("masks/maskscarton_5_pos1.jpg", dest1);
 
 		drawContours(dest2, cnts, 0, Scalar(0, 0, 255), 5);
-		//cornerHarris(dest2, corners, 2, 3, 0.04);
-//goodFeaturesToTrack(dest2, corners, 1, 0.5, 50, noArray(), 3, true);
 
-
-//display the points of the polygons. This is what we use to estimate the dimensions
+		//display the points of the polygons. This is what we use to estimate the dimensions
 
 		vector<Point> gPoints;
-		gPoints = selectGoodPointsCam2(cnts_poly);
-		for (int j = 0; j < gPoints.size(); j++)
+		//gPoints = selectGoodPointsCam2(cnts_poly);
+		for (int j = 0; j < cnts_poly.size(); j++)
 		{
-			circle(dest2, gPoints[j], 5, Scalar(0, 255, 0), 3);
+			circle(dest2, cnts_poly[j], 5, Scalar(0, 255, 0), 3);
+			cout << cnts_poly[j];
 		}
 		//circle(dest2, Point(230, 394), 5, Scalar(0, 255, 0), 3);
 
@@ -558,13 +564,141 @@ int treat_and_display_with_no_transfo()
 		return 0;
 	}
 	return 0;
-
 }
 
+int homographyPlane1()
+{
+	Mat image1, image2, image3;
+	image1 = imread("goodPictures/carton_1_pos1.jpg");
+	image2 = imread("goodPictures/carton_1_pos2.jpg");
+	image3 = imread("goodPictures/carton_3_pos1.jpg");
+
+	//cropping image2
+	Mat ROI(image2, Rect(0, 120, 640, 360));
+	ROI.copyTo(image2);
+
+
+	//To make the homography and calculate dimensions of plan 1
+	//Because Homography is the same, you can keep the matrix for the boxes. Just initialize on the biggest Box.
+
+	vector<Point3f> p1Points;
+
+	vector<Point2f> img1_pts;
+	vector<Point2f> img2_pts;
+
+	img2_pts.push_back(Point2f(0, 0));
+
+	img2_pts.push_back(Point2f(240, 0));
+	img2_pts.push_back(Point2f(0, 160));
+	img2_pts.push_back(Point2f(240, 160));
+
+	img1_pts.push_back(Point2f(344, 209));
+	img1_pts.push_back(Point2f(457, 197));
+	img1_pts.push_back(Point2f(350, 287));
+	img1_pts.push_back(Point2f(453, 306));
+
+	Mat H = findHomography(img1_pts, img2_pts);
+	cout << H;
+
+	Mat img_wrp, img_wrp2;
+
+	warpPerspective(image1, img_wrp, H, image1.size());
+	warpPerspective(image3, img_wrp2, H, image3.size());
+
+	imwrite("image_wrap.jpg", img_wrp);
+
+	namedWindow("display 1");
+	namedWindow("display 2");
+
+	imshow("display 1", img_wrp2);
+	imshow("display 2", img_wrp);
+
+	waitKey();
+	return 0;
+}
+
+void computeC2MC1(const Mat& R1, const Mat& tvec1, const Mat& R2, const Mat& tvec2,
+	Mat& R_1to2, Mat& tvec_1to2)
+{
+	//c2Mc1 = c2Mo * oMc1 = c2Mo * c1Mo.inv()
+	R_1to2 = R2 * R1.t();
+	tvec_1to2 = R2 * (-R1.t() * tvec1) + tvec2;
+}
+
+int homograpyPlane2()
+{
+	Mat image1, image2, image3;
+	image1 = imread("goodPictures/carton_1_pos1.jpg");
+	image2 = imread("goodPictures/carton_1_pos2.jpg");
+	image3 = imread("goodPictures/carton_3_pos1.jpg");
+
+	//cropping image2
+	Mat ROI(image2, Rect(0, 120, 640, 360));
+	ROI.copyTo(image2);
+
+	Mat cameraMatrix = (Mat_<float>(3, 3) << 605.57, 0, 334.90, 0, 591.46, 238.77, 0, 0, 1);
+	//To make the homography and calculate dimensions of plan 1
+	//Because Homography is the same, you can keep the matrix for the boxes. Just initialize on the biggest Box.
+
+	vector<Point3f> p1Points;
+
+	vector<Point2f> img1_pts;
+	vector<Point2f> img2_pts;
+
+	img2_pts.push_back(Point2f(0, 0));
+
+	img2_pts.push_back(Point2f(160, 0));
+	img2_pts.push_back(Point2f(0, 120));
+	img2_pts.push_back(Point2f(160, 120));
+
+	img1_pts.push_back(Point2f(457, 197));
+	img1_pts.push_back(Point2f(587, 198));
+	img1_pts.push_back(Point2f(453, 306));
+	img1_pts.push_back(Point2f(590, 283));
+
+
+	Mat H = findHomography(img1_pts, img2_pts);
+	cout << H;
+
+	Mat img_wrp, img_wrp2;
+
+	warpPerspective(image3, img_wrp, H, image1.size());
+	//warpPerspective(image3, img_wrp2, H, image3.size());
+
+	imwrite("image_wrap.jpg", img_wrp);
+
+	namedWindow("display 1");
+	namedWindow("display 2");
+
+	imshow("display 1", image3);
+	imshow("display 2", img_wrp);
+
+	waitKey();
+	//homographyPlane1();
+
+	cout << "Execution complete" << endl;
+
+	//treat_and_display_with_no_transfo();
+
+	return 0;
+}
 
 int main()
 {
-	treat_and_display_with_no_transfo();
+	//homographyPlane1();
+
+	VideoCapture cap = VideoCapture(1);
+	Mat frame;
+
+	cap >> frame;
+
+	namedWindow("display");
+	imshow("display", frame);
+	waitKey();
+
+	cout << "Execution complete" << endl;
+
+	//treat_and_display_with_no_transfo();
 
 	return 0;
 }
